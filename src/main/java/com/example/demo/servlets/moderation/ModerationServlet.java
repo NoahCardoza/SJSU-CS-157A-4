541b9e5926d237bc68b194d1a42a193eeac8f397
NoahCardoza <noahcardoza@gmail.com>
2023-11-27 00:16:39 -0800

    send email when user is banned

--- src/main/java/com/example/demo/servlets/moderation/ModerationServlet.java
+++ src/main/java/com/example/demo/servlets/moderation/ModerationServlet.java
@@ -1,5 +1,6 @@
 package com.example.demo.servlets.moderation;
 
+import com.example.demo.Emailer;
 import com.example.demo.Guard;
 import com.example.demo.Util;
 import com.example.demo.Validation;
@@ -55,7 +56,7 @@ public class ModerationServlet extends HttpServlet {
         response.sendRedirect(request.getContextPath() + "/moderation?f=users");
     }
 
-    private void users(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+    private void users(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException, InterruptedException {
         User currentUser = Guard.requireAuthenticationWithMessage(request, response, "You must be logged in to view the moderation panel.");
         if (currentUser == null) {
             return;
@@ -64,59 +65,74 @@ public class ModerationServlet extends HttpServlet {
 
         if (request.getMethod().equals("POST")) {
             Long userId = Long.parseLong(request.getParameter("user_id"));
+
+            if (userId == null) {
+                response.sendRedirect(request.getContextPath() + "/moderation?f=users");
+                return;
+            }
+
+            Optional<User> user = UserDao.getInstance().get(userId);
+
+            if (!user.isPresent()) {
+                response.sendRedirect(request.getContextPath() + "/moderation?f=users");
+                return;
+            }
+
             String action = request.getParameter("action");
-            if (action.equals("ban")) {
-                UserDao.getInstance().ban(userId);
-            } else if (action.equals("unban")) {
-                UserDao.getInstance().unban(userId);
-            } else if (action.equals("role_change")) {
-                Integer role = Integer.parseInt(request.getParameter("role"));
 
-                if (role == null) {
-                    response.sendRedirect(request.getContextPath() + "/moderation?f=users");
-                    return;
-                }
-
-                if (currentUser.getId().equals(userId)) {
-                    request.getSession().setAttribute("alert", new Alert("danger", "You cannot change your own role."));
-                    response.sendRedirect(request.getContextPath() + "/moderation?f=users");
-                    return;
-                }
-
-                Optional<User> user = UserDao.getInstance().get(userId);
-
-                if (!user.isPresent()) {
-                    response.sendRedirect(request.getContextPath() + "/moderation?f=users");
-                    return;
-                }
-
-                if (user.get().isAdministrator() || !currentUser.getId().equals(1)) {
-                    request.getSession().setAttribute("alert", new Alert("danger", "The super-admin is the only one who can change the role of other admins."));
-                    return;
-                }
-
-                // TODO: verify role changes are allowed
-                //       for now we are trusting that admins/mods
-                //       play fair
-
-                // TODO: remove flags in favor of string roles "admin" and "moderator"
-
-                switch (role) {
-                    case 1:
-                        UserDao.getInstance().demote(userId, "administrator");
-                        UserDao.getInstance().demote(userId, "moderator");
+            switch (action) {
+                case "ban" -> {
+                    if (currentUser.getId().equals(userId)) {
+                        request.setAttribute("alert", new Alert("danger", "You cannot ban yourself."));
                         break;
-                    case 2:
-                        UserDao.getInstance().promote(userId, "moderator");
-                        UserDao.getInstance().demote(userId, "administrator");
+                    }
+                    if (user.get().isAdministrator() && !currentUser.getId().equals(1L)) {
+                        request.setAttribute("alert", new Alert("danger", "The super-admin is the only one who can ban other admins."));
                         break;
-                    case 3:
-                        UserDao.getInstance().demote(userId, "moderator");
-                        UserDao.getInstance().promote(userId, "administrator");
-                        break;
-                    default:
+                    }
+
+                    UserDao.getInstance().ban(userId);
+                    Emailer.sendBanNotice(user.get());
+                }
+                case "unban" -> UserDao.getInstance().unban(userId);
+                case "role_change" -> {
+                    Integer role = Util.parseIntOrNull(request.getParameter("role"));
+                    if (role == null) {
                         response.sendRedirect(request.getContextPath() + "/moderation?f=users");
                         return;
+                    }
+                    if (currentUser.getId().equals(userId)) {
+                        request.setAttribute("alert", new Alert("danger", "You cannot change your own role."));
+                        break;
+                    }
+                    if (user.get().isAdministrator() && !currentUser.getId().equals(1L)) {
+                        request.setAttribute("alert", new Alert("danger", "The super-admin is the only one who can change the role of other admins."));
+                        break;
+                    }
+
+                    // TODO: verify role changes are allowed
+                    //       for now we are trusting that admins/mods
+                    //       play fair
+
+                    // TODO: remove flags in favor of string roles "admin" and "moderator"
+
+                    switch (role) {
+                        case 1 -> {
+                            UserDao.getInstance().demote(userId, "administrator");
+                            UserDao.getInstance().demote(userId, "moderator");
+                        }
+                        case 2 -> {
+                            UserDao.getInstance().promote(userId, "moderator");
+                            UserDao.getInstance().demote(userId, "administrator");
+                        }
+                        case 3 -> {
+                            UserDao.getInstance().demote(userId, "moderator");
+                            UserDao.getInstance().promote(userId, "administrator");
+                        }
+                        default -> {
+                            request.setAttribute("alert", new Alert("danger", "Invalid role."));
+                        }
+                    }
                 }
             }
         }
