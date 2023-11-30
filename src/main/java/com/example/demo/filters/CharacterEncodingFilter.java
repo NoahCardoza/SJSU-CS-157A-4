7374f38b13210eae0fe082597ded94e2a9504f43
NoahCardoza <noahcardoza@gmail.com>
2023-11-29 16:24:52 -0800

    fix content types

--- src/main/java/com/example/demo/filters/CharacterEncodingFilter.java
+++ src/main/java/com/example/demo/filters/CharacterEncodingFilter.java
@@ -9,18 +9,17 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig)
             throws ServletException {
-
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        servletRequest.setCharacterEncoding("UTF-8");
-        servletResponse.setContentType("text/html; charset=UTF-8");
         filterChain.doFilter(servletRequest, servletResponse);
+        if (servletRequest.getContentType() != null && servletRequest.getContentType().startsWith("text/html")) {
+            servletRequest.setCharacterEncoding("UTF-8");
+            servletResponse.setContentType("text/html; charset=UTF-8");
+        }
     }
 
     @Override
-    public void destroy() {
-
-    }
+    public void destroy() {}
 }
